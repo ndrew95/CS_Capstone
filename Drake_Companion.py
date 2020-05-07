29a3015a42ea382031d699ecfb065f310a26d568
ndrew95 <44056677+ndrew95@users.noreply.github.com>
2020-05-07 12:50:02 -0500

    Update Drake_Companion.py
    There is a lot of unused code, but it is working with database integration

--- Drake_Companion.py
+++ Drake_Companion.py
@@ -5,6 +5,36 @@ from kivy.properties import StringProperty
 from kivy.uix.screenmanager import Screen, SlideTransition
 import os
 from kivy.uix.checkbox import CheckBox
+from kivy.utils import platform
+if platform == "ios":
+    from os.path import join, dirname
+    import kivy.garden
+    kivy.garden.garden_app_dir = join(dirname(__file__), "libs", "garden")
+from kivy.garden.mapview import MapView
+from kivy.garden.mapview import MapMarkerPopup
+from kivy.uix.bubble import Bubble
+from kivy.garden.mapview import MapView, MapMarker
+from datetime import datetime
+from datetime import timedelta
+from datetime import date
+import pymysql
+import pymysql.cursors
+import time
+from kivy.clock import Clock
+
+host='cap-comp.cpwue0appyn7.us-west-2.rds.amazonaws.com'
+port=3306
+dbname= 'Companion'
+user="admin234"
+password="tf7A8sjX#!"
+
+#hour = datetime.datetime.now().hour
+#minutes = datetime.datetime.now().minute
+
+conn = pymysql.connect(db=dbname,host=host, password=password, port=port, user=user)
+cursor = conn.cursor()
+
+#from kivy.garden.mapview.MapMarkerPopup import MapMarkerPopup 
 
 class MainWindow(Screen):
     def do_login(self, loginText, passwordText):
@@ -25,18 +55,166 @@ class MainWindow(Screen):
 
 
 class SecondWindow(Screen):
+    
     tab_pos = 'top_mid'
-  
+    #test = SecondWindow()
+    #test.update_events()
     event_update = StringProperty()
     def __init__(self, **kwargs):
         super(SecondWindow, self).__init__(**kwargs)
         #this self.event_update having the value of 'new event' is for purposes of showing the utility of the
         #refresh button
-        self.event_update = str('new event')
-    
-    #self.event_update = str('new event')
+        #lock.schedule_once(self.update_txt, 0.1)
 
-    def update_events(self):
+    #def update_txt(self, *args):
+       #count = 0
+       # if (count==1):
+       # #    pass
+        #else:
+         #   self.update_events()
+        
+    #self.event_update = str('new event')
+    def set_marker_position(self):
+       # marker = MapView.mar
+        map = MapView(zoom=17, lon=-93.655, lat=41.605)
+        mapview = MapView
+        m1 = MapMarker(lon=-93.655, lat=41.605)
+        map.add_marker(m1)
+        
+    def get_next_event(self):
+        cursor.execute('SELECT Description, time from EVENTS order by time')
+        next_event = cursor.fetchall()
+        #next_event = str(next_event).strip('(')
+        #next_event = str(next_event).strip('\'')
+        #next_event = str(next_event).strip(',')
+
+        '''
+        dateNow = datetime.datetime.utcnow()
+        dataNew = r['stream']['created_at']
+        dateStart = datetime.datetime.strptime(dataNew, '%Y-%m-%dT%H:%M:%SZ')
+        elapsedTime = dateNow - dateStart
+        elapsedTimeHours = str(elapsedTime).split(':')
+        elapsedTimeHours2 = elapsedTimeHours[0]
+        elapsedTimeMinutes = elapsedTimeHours[1]
+        '''
+
+        return(next_event)
+        #mapview.add_marker(MapMarkerPopup(lat=41.605,lon=-93.655, placeholder= Bubble()))
+    def time_to_event(self):
+        test = SecondWindow()
+
+        events = test.get_next_event()
+        dateNow = datetime.now().time()
+        #print(dateNow)
+        date1 = date(1, 1, 1)
+        datetime1 = datetime.combine(date1, dateNow)
+        #delta = timedelta(hours = 24)
+        #t = dateNow
+       # print(dateNow)
+       # dateNow1 = datetime.combine(datetime.date(1,1,1),t) + delta.time()
+       # print(dateNow1)
+        #date= date.replace(2018)
+        #timeNow = dateNow.time().strftime('%H:%M:%S')
+        #timeNow = datetime.datetime.now().timed()
+        '''
+        minTime = dateNow - events[1][1]
+        splitTime = str(minTime).split(' ')
+        splitTime1 = splitTime[1]
+        elapsedTimeHours = str(splitTime1).split(':')
+        elapsedTimeHours2 = elapsedTimeHours[0]
+        #elapsedTimeHours3 = elapsedTimeHours2.split(' ')
+        elapsedTimeMinutes = elapsedTimeHours[1]
+        #print(elapsedTimeHours2)
+
+        minTime = int(f"{elapsedTimeHours2}{elapsedTimeMinutes}")
+        '''
+        testList = []
+        testList1 = []
+        count = 0 
+        
+        for i in events:
+            #dataNew = i[1]
+            
+            #elapsedTime = dateNow - i[1]
+            #hoursNow = str(dateNow).split(' ')
+            
+            #hoursNow1 = hoursNow[1].split(':')
+            #hoursNow2 = hoursNow[1].split('.')
+            #print(type(hoursNow2[0]))
+            
+            dateStart1 = datetime.strptime(str(i[1]), '%H:%M:%S')
+            stop_time = datetime.time(dateStart1)
+            #print(stop_time)
+            datetime2 = datetime.combine(date1, stop_time)
+            #print(dateStart)
+            elapsedTime = datetime2 - datetime1
+
+            #print(i[1])
+            print(elapsedTime)
+            elapsedTimeHours = str(elapsedTime).split(':')
+            elapsedTimeHours2 = elapsedTimeHours[0]
+            elapsedTimeHours3 = elapsedTimeHours2.split(' ')
+            elapsedTimeMinutes = elapsedTimeHours[1]
+            #print(hoursNow2[0])
+            #print(elapsedTimeMinutes)
+            #print(dateNow)
+            #print(elapsedTime)
+            if "day" in str(elapsedTime):
+                                                                        
+                test = str(elapsedTime).split('day, ')
+                testList.append(str(test[1]))
+                print(test)
+            else:
+            #elapsedTime = (int(f"{elapsedTimeHours3[1]}{elapsedTimeMinutes}"))
+                testList.append(str(elapsedTime))
+                #print(testList[count])
+                #print(elapsedTime)
+                testList1.append(i[0])
+                count = count + 1
+
+       # for i in testList:
+          #  if "day" in i:
+            #    i.split('')
+        print(testList)
+        l = sorted(enumerate(testList), key=lambda i: i[1])[:4]
+        #m = sorted(range(len(testList)))[:4]
+        #testList.sort(reverse=True)
+        #testList1.sort(reverse=True)
+        count=0
+        print(l)
+        #print(testList1)
+        testList3 = []
+        for i in testList:
+            if "day" not in i:
+                testList3.append(i)
+            count = count + 1
+        #print(testList3)
+        #for i in testList3:
+            #print(i)
+
+            #print(testList.index(i))
+        #print(testList1[m[0]])
+        #print(testList1[5])
+        #print(events[35][0])
+        #print(m)
+        #testList.sort()
+        #print(testList)
+        #print(testList)
+        #print(l)
+       # print(l)
+        #print(testList)
+        #print(events[20][0])
+        return(events[l[0][0]][0], events[l[1][0]][0], events[l[2][0]][0])
+        #testList.sort()
+        #testList1.sort()
+        #print(testList)
+        #print(testList1)
+            #elapsedTimeHours = str(elapsedTime).split(':')
+            #elapsedTimeHours2 = elapsedTimeHours[0]
+            #elapsedTimeMinutes = elapsedTimeHours[1]
+           # print(elapsedTime)
+            #print(i[1])
+    def update_events(self,x):
         
         '''
         here we can have the number of events we want to display and dynamically change the events name
@@ -45,9 +223,10 @@ class SecondWindow(Screen):
         via code
         '''
         test = SecondWindow()
-        
+        test.time_to_event()
         events = self.ids.event1
         events2 = self.ids.event2
+        events3 = self.ids.event3
         
         '''
           Where to go from here:
@@ -64,22 +243,35 @@ class SecondWindow(Screen):
           users to obtain up-to-date information while remaining inside the application
       
         '''
-        events.text = 'test'
-        events2.text = 'test2'
-
+        events.text = test.time_to_event()[0]
+        events2.text = test.time_to_event()[1]
+        events3.text = test.time_to_event()[2]
+        #return(True)
     def reset_checkbox(self):
         for child in reversed(self.ids.two.children):
             if isinstance(child, CheckBox):
                 child.active = False    
                 
         
-    
+class DefaultWindow(Screen):
+    pass
 class CreateWindow(Screen):
+    pass
+class MapWindow(Screen):
     pass
 
 class WindowManager(ScreenManager):
     pass
 
+class PopUpWindow(Screen):
+    def set_marker_position(self):
+        
+        x, y = mapview.get_window_xy_from(marker.lat, marker.lon, mapview.zoom)
+        marker.x = int(x - marker.width * marker.anchor_x)
+        marker.y = int(y - marker.height * marker.anchor_y)
+        if isinstance(marker, MapMarkerPopup):
+            marker.placeholder.x = marker.x - marker.width / 2
+            marker.placeholder.y = marker.y + marker.height
 
 
 
