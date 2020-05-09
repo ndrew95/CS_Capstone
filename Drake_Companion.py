0541fd15d3782b2a75c95f64893cb4210fb674b4
ndrew95 <44056677+ndrew95@users.noreply.github.com>
2020-05-09 09:41:04 -0500

    Update Drake_Companion.py

--- Drake_Companion.py
+++ Drake_Companion.py
@@ -14,14 +14,21 @@ from kivy.garden.mapview import MapView
 from kivy.garden.mapview import MapMarkerPopup
 from kivy.uix.bubble import Bubble
 from kivy.garden.mapview import MapView, MapMarker
-
 from datetime import datetime
 from datetime import timedelta
 from datetime import date
 import pymysql
+from kivy.uix.button import Button
+from kivy.uix.layout import Layout
+#import kivy
+from kivy.uix.label import Label
+from  kivy.core.window import Window
 import pymysql.cursors
 import time
 from kivy.clock import Clock
+#import Label
+from kivy.uix.popup import Popup
+from kivy.uix.gridlayout import GridLayout
 
 host='cap-comp.cpwue0appyn7.us-west-2.rds.amazonaws.com'
 port=3306
@@ -35,22 +42,61 @@ password="tf7A8sjX#!"
 conn = pymysql.connect(db=dbname,host=host, password=password, port=port, user=user)
 cursor = conn.cursor()
 
-
 #from kivy.garden.mapview.MapMarkerPopup import MapMarkerPopup 
 
 class MainWindow(Screen):
+    
     def do_login(self, loginText, passwordText):
         app = App.get_running_app()
-
+        cursor.execute('SELECT User, Pass from LOGIN321')
+        logins = cursor.fetchall()
         app.username = loginText
         app.password = passwordText
+        global login123
+        login123 =loginText
+        
+        #login123 = ''
+        layout = GridLayout(cols = 1)
+        #popupLabel = Label(text = "Incorrect Login Information") 
+        closeButton = Button(text = "Close") 
+  
+        #layout.add_widget(popupLabel) 
+        layout.add_widget(closeButton)
 
-        self.manager.transition = SlideTransition(direction="left")
-        self.manager.current = 'second'
+        popup = Popup(title ='Incorrect Login Information', 
+                      content = layout,
+                      pos_hint={'x': 600.0 / Window.width, 
+                            'y':600.0 /  Window.height},
+                      size_hint=(None, None), size=(500,200))   
+        #popup.open()    
+  
+        # Attach close button press with popup.dismiss action 
+        #closeButton.bind(on_press = popup.dismiss)
+        #print(logins[0][1])
+        checkUser = False
+        count = 0
+        for i in logins:
+            print(i[1])
+            if i[0] == app.username and i[1] == app.password:
+                checkUser = True
+                break
+        
+
+       # print(app.username)
+        if checkUser == True:
+
+            self.manager.transition = SlideTransition(direction="left")
+            self.manager.current = 'second'
+        else:
+            popup.open() 
+            closeButton.bind(on_press = popup.dismiss)
 
         app.config.read(app.get_application_config())
         app.config.write()
-
+    def login_user(self):
+        login123 = self.ids.login.text
+        print(login123)
+        return(login123)
     def resetForm(self):
         self.ids['login'].text = ""
         self.ids['password'].text = ""
@@ -83,10 +129,10 @@ class SecondWindow(Screen):
         m1 = MapMarker(lon=-93.655, lat=41.605)
         map.add_marker(m1)
         
-
     def get_next_event(self):
-        cursor.execute('SELECT Description, time from EVENTS order by time')
+        cursor.execute('SELECT Description, Time, EventID from EVENTS order by time')
         next_event = cursor.fetchall()
+        #cursor.close()
         #next_event = str(next_event).strip('(')
         #next_event = str(next_event).strip('\'')
         #next_event = str(next_event).strip(',')
@@ -153,7 +199,7 @@ class SecondWindow(Screen):
             elapsedTime = datetime2 - datetime1
 
             #print(i[1])
-            print(elapsedTime)
+            #print(elapsedTime)
             elapsedTimeHours = str(elapsedTime).split(':')
             elapsedTimeHours2 = elapsedTimeHours[0]
             elapsedTimeHours3 = elapsedTimeHours2.split(' ')
@@ -166,7 +212,7 @@ class SecondWindow(Screen):
                                                                         
                 test = str(elapsedTime).split('day, ')
                 testList.append(str(test[1]))
-                print(test)
+                #print(test)
             else:
             #elapsedTime = (int(f"{elapsedTimeHours3[1]}{elapsedTimeMinutes}"))
                 testList.append(str(elapsedTime))
@@ -178,13 +224,23 @@ class SecondWindow(Screen):
        # for i in testList:
           #  if "day" in i:
             #    i.split('')
-        print(testList)
-        l = sorted(enumerate(testList), key=lambda i: i[1])[:4]
+        #print(testList)
+        testList6 = [None] * len(testList)
+        count = 0
+        for i in testList:
+            testList5=i.split(":")
+            testList6[count] =(int(f"{testList5[0]}{testList5[1]}"))
+            count = count +1
+        #print(testList6)
+        l = sorted(enumerate(testList6), key=lambda i: i[1])
         #m = sorted(range(len(testList)))[:4]
         #testList.sort(reverse=True)
+
         #testList1.sort(reverse=True)
         count=0
-        print(l)
+        #testList.sort()
+        #print(testList)
+        #print(l)
         #print(testList1)
         testList3 = []
         for i in testList:
@@ -194,7 +250,10 @@ class SecondWindow(Screen):
         #print(testList3)
         #for i in testList3:
             #print(i)
-
+       # print(events[0][2])
+        #print(l)
+        #print(events[l[len(l)-1][0]][2])
+        #print(events[l[len(l)-2][0]][2])
             #print(testList.index(i))
         #print(testList1[m[0]])
         #print(testList1[5])
@@ -207,7 +266,9 @@ class SecondWindow(Screen):
        # print(l)
         #print(testList)
         #print(events[20][0])
-        return(events[l[0][0]][0], events[l[1][0]][0], events[l[2][0]][0])
+        #cursor.close()
+        print(events[0][1])
+        return(events[l[0][0]][0], events[l[1][0]][0], events[l[2][0]][0], events[l[len(l)-1][0]][2], events[l[len(l)-2][0]][2], events[l[len(l)-3][0]][2], events[l[0][0]][1],events[l[1][0]][1], events[l[2][0]][1])
         #testList.sort()
         #testList1.sort()
         #print(testList)
@@ -217,9 +278,43 @@ class SecondWindow(Screen):
             #elapsedTimeMinutes = elapsedTimeHours[1]
            # print(elapsedTime)
             #print(i[1])
+    def update_results(self,x):
+        
+        '''
+        here we can have the number of events we want to display and dynamically change the events name
+        when the app boots up, so it would also be useful to have a refresh button. The next goal for this would
+        be to have the event name update by hooking up to the database, and not have to be entered manually
+        via code
+        '''
+        test = SecondWindow()
+        test.time_to_event()
+        events = self.ids.result1
+        events2 = self.ids.result2
+        events3 = self.ids.result3
+        query = """SELECT EVENTS.Description, ATHLETE.FirstName, ATHLETE.LastName, RESULTS.AthleteRank, SCHOOL.SchoolName from RESULTS, ATHLETE, EVENTS, SCHOOL WHERE RESULTS.EventID = '%s' AND RESULTS.AthleteID = ATHLETE.AthleteID AND RESULTS.EventID = EVENTS.EventID AND ATHLETE.SchoolID = SCHOOL.SchoolID""" %(test.time_to_event()[3])
+        cursor.execute(query)
+        next_result = cursor.fetchall()
+        #$resultID = test.time_to_event()[3]
+        
+        query = """SELECT EVENTS.Description, ATHLETE.FirstName, ATHLETE.LastName, RESULTS.AthleteRank, SCHOOL.SchoolName from RESULTS, ATHLETE, EVENTS, SCHOOL WHERE RESULTS.EventID = '%s' AND RESULTS.AthleteID = ATHLETE.AthleteID AND RESULTS.EventID = EVENTS.EventID AND ATHLETE.SchoolID = SCHOOL.SchoolID""" %(test.time_to_event()[4])
+        cursor.execute(query)
+        next_result1 = cursor.fetchall()
+        query = """SELECT EVENTS.Description, ATHLETE.FirstName, ATHLETE.LastName, RESULTS.AthleteRank, SCHOOL.SchoolName from RESULTS, ATHLETE, EVENTS, SCHOOL WHERE RESULTS.EventID = '%s' AND RESULTS.AthleteID = ATHLETE.AthleteID AND RESULTS.EventID = EVENTS.EventID AND ATHLETE.SchoolID = SCHOOL.SchoolID""" %(test.time_to_event()[5])
+        cursor.execute(query)
+        next_result2 = cursor.fetchall()
+        #print(next_result[0][0])
+        #print("time to event: " + str(test.time_to_event()[3]))
+        
+        events.text = str(next_result[0][0]) + " Winner:  " + str(next_result[0][1]) + " " + str(next_result[0][2] + " of " + str(next_result[0][4]))
+        events2.text = str(next_result1[0][0]) + " Winner:  " + str(next_result1[0][1]) + " " + str(next_result1[0][2] + " of " + str(next_result1[0][4]))
+        events3.text = str(next_result2[0][0]) + " Winner:  " + str(next_result2[0][1]) + " " + str(next_result2[0][2] + " of " + str(next_result2[0][4]))
+
+        #cursor.close()
+        #events2.text = test.time_to_event()[4]
+        #events3.text = test.time_to_event()[5]
+        #events2.text = test.time_to_event()[4]
+        #events3.text = test.time_to_event()[5]
     def update_events(self,x):
-
-
         
         '''
         here we can have the number of events we want to display and dynamically change the events name
@@ -232,7 +327,9 @@ class SecondWindow(Screen):
         events = self.ids.event1
         events2 = self.ids.event2
         events3 = self.ids.event3
-        
+        time = self.ids.time1
+        time1 = self.ids.time2
+        time2 = self.ids.time3
         '''
           Where to go from here:
       
@@ -251,32 +348,153 @@ class SecondWindow(Screen):
         events.text = test.time_to_event()[0]
         events2.text = test.time_to_event()[1]
         events3.text = test.time_to_event()[2]
+        time.text = str(test.time_to_event()[6])
+        time1.text = str(test.time_to_event()[7])
+        time2.text = str(test.time_to_event()[8])
+
         #return(True)
     def reset_checkbox(self):
+        test = SecondWindow()
+        test1 = MainWindow()
+        #userName1 = test1.login_user()
+        print(login123)
+        layout = GridLayout(cols = 1)
+        #popupLabel = Label(text = "Incorrect Login Information") 
+        closeButton = Button(text = "Close") 
+  
+        #layout.add_widget(popupLabel) 
+        layout.add_widget(closeButton)
+
+        popup = Popup(title ='Already Submitted Interest for this Event', 
+                      content = layout,
+                      pos_hint={'x': 600.0 / Window.width, 
+                            'y':600.0 /  Window.height},
+                      size_hint=(None, None), size=(500,200)) 
+        interest1 = False
+        interest2 = False
+        interest3 = False
+       # print(login123)
+        #app = App.get_running_app()
+        #print(app.ids.login.text)
+        sql = "SELECT User, Event FROM DUPLICATE"
+        cursor.execute(sql)
+        duplicate = cursor.fetchall()
+        print(duplicate[0])
+        for i in duplicate:
+            if i[0] == login123 and i[1] == test.time_to_event()[0]:
+                interest1=True 
+            elif i[0] == login123 and i[1] == test.time_to_event()[1]:
+                interest2=True
+            elif i[0] == login123 and i[1] == test.time_to_event()[2]:
+                interest3=True
+        
+        if interest1 == True:
+            layout = GridLayout(cols = 1)
+        #popupLabel = Label(text = "Incorrect Login Information") 
+            closeButton = Button(text = "Close") 
+      
+            #layout.add_widget(popupLabel) 
+            layout.add_widget(closeButton)
+
+            popup = Popup(title ='Already Submitted Interest for this Event', 
+                      content = layout,
+                      pos_hint={'x': 600.0 / Window.width, 
+                            'y':600.0 /  Window.height},
+                      size_hint=(None, None), size=(500,200)) 
+        check1 = self.ids.check1
+        check2 = self.ids.check2
+        check3 = self.ids.check3
+        test.time_to_event()
+        if(check1.active==True and interest1==False):
+            sql = """INSERT INTO INTEREST (Description) VALUES('%s')"""%(test.time_to_event()[0])
+            cursor.execute(sql)
+            sql = """INSERT INTO DUPLICATE (User, Event) VALUES('%s', '%s')"""%(login123, test.time_to_event()[0])
+            cursor.execute(sql)
+            #interest1 = True
+            print('here')
+            conn.commit()
+        elif(check1.active==True and interest1==True):
+            popup.open() 
+            closeButton.bind(on_press = popup.dismiss)
+            
+
+            #print(test.time_to_event()[0])
+        if(check2.active==True and interest2==False):
+            sql = """INSERT INTO INTEREST (Description) VALUES('%s')"""%(test.time_to_event()[1])
+            cursor.execute(sql)
+            sql = """INSERT INTO DUPLICATE (User, Event) VALUES('%s', '%s')"""%(login123, test.time_to_event()[1])
+            cursor.execute(sql)
+            #interest2 = True
+            conn.commit()
+        elif(check2.active==True and interest2==True):
+            popup.open() 
+            closeButton.bind(on_press = popup.dismiss)
+        if(check3.active==True and interest3==False):
+            sql = """INSERT INTO INTEREST (Description) VALUES('%s')"""%(test.time_to_event()[2])
+            cursor.execute(sql)
+            sql = """INSERT INTO DUPLICATE (User, Event) VALUES('%s', '%s')"""%(login123, test.time_to_event()[2])
+            cursor.execute(sql)
+            #interest3 = True
+            conn.commit()
+        elif(check3.active==True and interest3==True):
+            popup.open() 
+            closeButton.bind(on_press = popup.dismiss)
         for child in reversed(self.ids.two.children):
             if isinstance(child, CheckBox):
-                child.active = False    
-                
+                child.active = False
+
+    def update_interest(self,x): 
+        test = SecondWindow()
+        sql = "SELECT Description, COUNT(*) occurences FROM INTEREST GROUP BY Description HAVING COUNT(*)>1 ORDER BY occurences DESC "#i#nsertStatement = "INSERT INTO LOGIN321  (User, Pass)   VALUES('test', 'user')"
+        cursor.execute(sql)
+        interest = cursor.fetchall()
+        print(interest[1][0])
+        events1 = self.ids.interest1
+        events2 = self.ids.interest2
+        events3 = self.ids.interest3
+        try:
+            events1.text = interest[0][0]
+        except:
+            x = 1
+        try:
+            events2.text = interest[1][0]
+        except:
+            x=1
+        try:
+            events3.text = interest[2][0]
+        except:
+            x=1
+
+        #events1.text = 
         
 class DefaultWindow(Screen):
     pass
 class CreateWindow(Screen):
-    pass
+    def Create_Account(self,loginText, passwordText):
+        app = App.get_running_app()
+        app.username = loginText
+        app.password = passwordText
+        sql = """INSERT INTO LOGIN321 (User, Pass) VALUES('%s','%s')"""%(app.username, app.password)
+        cursor.execute(sql)
+        conn.commit()
+        #cursor.execute('INSERT INTO LOGIN321 (User, Pass) VALUES('%s')'
 class MapWindow(Screen):
-    pass
+    def set_marker_position(self,x):
+        
+        map = MapView(zoom=11, lat=41.605, lon=-93.655, double_tap_zoom = True)
+        marker_1 = MapMarker(lon=-93.655, lat=41.605)
+        map.add_marker(marker_1)
 
 class WindowManager(ScreenManager):
     pass
 
 class PopUpWindow(Screen):
-    def set_marker_position(self):
+    def set_marker_position(self,x):
         
-        x, y = mapview.get_window_xy_from(marker.lat, marker.lon, mapview.zoom)
-        marker.x = int(x - marker.width * marker.anchor_x)
-        marker.y = int(y - marker.height * marker.anchor_y)
-        if isinstance(marker, MapMarkerPopup):
-            marker.placeholder.x = marker.x - marker.width / 2
-            marker.placeholder.y = marker.y + marker.height
+        map = MapView(zoom=11, lat=41.605, lon=-93.655, double_tap_zoom = True)
+        marker_1 = MapMarker(lat=41.605, lon=-93.655)
+        map.add_marker(marker_1)
+        #return map
 
 
 
